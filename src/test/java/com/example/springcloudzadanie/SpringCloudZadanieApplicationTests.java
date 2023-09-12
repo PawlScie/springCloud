37c68a2b651b5a0a794ed02163d205e01341dcc6
Pawlak <polo1945@wp.pl>
2023-09-12 14:25:24 +0200

    Zadanie

---
+++ src/test/java/com/example/springcloudzadanie/SpringCloudZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudzadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudZadanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
